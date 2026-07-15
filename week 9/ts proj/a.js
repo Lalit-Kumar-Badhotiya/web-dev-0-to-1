5c142fcca60294895aa38c263d4ddad6c84815d4
Lalit-Kumar-Badhotiya <lalitkumar.ac106@gmail.com>
2026-07-15 13:15:34 +0530

    added week 9 typescript intro and ts compiler theory and all that jazz

---
+++ week 9/ts proj/a.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isLegal() {
+    console.log("hello");
+}
+//# sourceMappingURL=a.js.map
---
+++ week 9/ts proj/b.ts
@@ -0,0 +1,3 @@
+function isLegal(){
+    console.log("hello")
+}
